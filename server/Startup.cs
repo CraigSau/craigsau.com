2d115ca808123650db637da9db4457be8e4975ef
Craig <craigsauers@ymail.com>
2023-09-10 10:12:19 -0500

    Trying to get website deployed, very basic front end

---
+++ server/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+
+
+namespace server;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        this.Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        //services.addDbCont
+    }
+
+}

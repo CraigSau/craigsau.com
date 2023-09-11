def40028cae3d259af2a4886aaa4877dd974f430
Craig <craigwsauers@gmail.com>
2023-09-11 15:31:33 -0500

    Prepping for connecting to DB

---
+++ server.database/MySqlDatabase.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace server.database;
+// https://developer.okta.com/blog/2019/08/02/build-a-simple-crud-app-with-aspnetcore-mysql-and-twilio
+public class MySqlDatabase
+{
+
+}
---
+++ server.database/dbo/Tables/User.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE User
+(
+    [User_ID] BIGINT
+)

4e42e25ba66e172dba09aa23cea76ff221bf2889
Craig <craigwsauers@gmail.com>
2023-10-08 13:30:19 -0500

    Beginning to implement tailwind

---
+++ client/tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    "./views/*.{html, js}",
+    "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
+
